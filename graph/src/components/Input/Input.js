0c76a73e5f57513a743ce6fe22bac215d92ace4e
Andrew Miller <jazznerd206@icloud.com>
2021-04-12 09:34:05 -0700

    trie completegit add . just needs the real words

--- graph/src/components/Input/Input.js
+++ graph/src/components/Input/Input.js
@@ -6,16 +6,13 @@ function Input(props) {
 
     const [ value, setValue ] = useState('');
 
-    // console.log(`Trie`, Trie);
-
-    // useEffect(() => {
-    //     CreateTrie(value);
-    // }, [value])
+    const [ dropdownList, setDropdownList ] = useState([])
 
     const onChange = event => {
         event.preventDefault();
         let sList = props.onSearch(props.trie, event.target.value);
         console.log(`List of symbols generated from trie: ${sList}`);
+        setDropdownList(sList);
         setValue(event.target.value);
     }
 
@@ -23,12 +20,19 @@ function Input(props) {
         <div className="symbol-form">
             <form>
                 <label htmlFor="symbol" className="label">Symbol: </label>
-                <input
-                    className=""
-                    type="input"
-                    name="symbol"
-                    value={value}
-                    onChange={(e) => onChange(e)} />
+                    <input
+                        className=""
+                        type="input"
+                        list="symbol"
+                        name="symbol"
+                        value={value}
+                        onChange={(e) => onChange(e)}
+                    />
+                    <datalist id="symbol" name="symbolDD">
+                        {dropdownList.map((symbol, index) => {
+                            return <option key={index} value={symbol}>{symbol}</option>
+                        })}
+                    </datalist>
                 <button type="submit" className='btn btn-success' onClick={e => props.onClick(e, value)}>Get quote</button>
             </form>
         </div>
