2335bb9b92e92185a0dac73814cb6ca7f29a4d0d
Andrew Miller <jazznerd206@icloud.com>
2021-08-30 08:57:18 -0700

    remove scroll bar

--- graph/src/basics/global.theme.jsx
+++ graph/src/basics/global.theme.jsx
@@ -6,6 +6,20 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     font-family: '${props => props.theme.font}';
     color: '${props => props.theme.color}';
+    overflow: auto;
+    ::-webkit-scrollbar {
+      display: none;
+      width: 10px;
+      background-color: ${props => props.theme.bgalt};
+    }
+    ::-webkit-scrollbar-track {
+      width: 10px;
+      background-color: ${props => props.theme.bgalt};
+    }
+    ::-webkit-scrollbar-thumb {
+      -webkit-box-shadow: inset 0 0 6px ${props => props.theme.bg};
+	    border-radius: 10px;
+    }
   }
   body {
     position: relative;
