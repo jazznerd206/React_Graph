ab8b35a1597bb221b68f39aac0ae42ab139a1c88
Andrew Miller <206alm@gmail.com>
2021-09-14 13:18:04 -0700

    error boundaries

--- graph/src/components/CompanyData/tabComponents/Summary.jsx
+++ graph/src/components/CompanyData/tabComponents/Summary.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { DP } from '../components/layout/current.layout'
 import { Down, P, Up } from '../../../basics/basic.layout'
 
-function Summary(props) {
-    return (
+function Summary({data}) {
+    if (data.description === undefined || data.description === null) return <div>Loading...</div>
+    else return (
         <>
-            <P>{props.data.description.length > 512 ? props.data.description.substring(0, 512): props.data.description}{'...'}</P>
+            <P>{data.description.length > 512 ? data.description.substring(0, 512): data.description}{'...'}</P>
         </>
     )
 }
